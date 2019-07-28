3c77bf37022b84afea9548b328a7b9e068060a02
qinjiyuan <jiyuan.qin@foxmail.com>
2019-07-28 19:49:28 +0800

    一些修改

--- src/main/java/top/mixedinfos/reports/ReportsApplication.java
+++ src/main/java/top/mixedinfos/reports/ReportsApplication.java
@@ -3,6 +3,7 @@ package top.mixedinfos.reports;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ReportsApplication {
 
